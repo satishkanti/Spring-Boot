f5471d25536b0b7456956ce51f33cd975ab2dfff
Satish Kanti <satishkanti22@gmail.com>
2023-10-09 13:36:23 +0530

    initial commit

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/kanti/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/kanti/demo/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.kanti.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +42,14 @@ public class StudentRestController {
 
         // just index into the list ... keep it simple for now 
 
+        // check the studentsId against the list size
+        if((studentId >= theStudents.size()) || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+         
         return theStudents.get(studentId);
     }
+
+    // Add an exception handler using @ExceptionHandler
+   
 }
