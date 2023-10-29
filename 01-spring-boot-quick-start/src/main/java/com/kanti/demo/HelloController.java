f99baa152e3cdd40c3f2229776f477543c1b5227
Satish Kanti <satishkanti22@gmail.com>
2023-10-29 22:57:05 +0530

    initial commit

---
+++ 01-spring-boot-quick-start/src/main/java/com/kanti/demo/HelloController.java
@@ -0,0 +1,37 @@
+package com.kanti.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name:" + teamName;
+    }
+    
+    @GetMapping("/")
+    public String Hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+
+}
